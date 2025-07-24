70ab9dcf1e8d5c657c94ce6a5386be3d0ae11d97
FadilPng <anggaraalig6969@gmail.com>
2025-07-24 13:44:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,18 @@ function nextSlide() {
 function prevSlide() {
     showSlide(currentIndex - 1);
     }
+
+    function submitForm(event) {
+      event.preventDefault(); // cegah reload halaman
+      const nama = document.getElementById("nama").value;
+      const email = document.getElementById("email").value;
+      const prodi = document.getElementById("prodi").value;
+
+      alert("Pendaftaran berhasil!\n\nNama: " + nama + "\nEmail: " + email + "\nProgram Studi: " + prodi);
+
+      // Reset form setelah submit
+      event.target.reset();
+    }
   
 // untuk pindah gambarnya otomatis
 setInterval(() => {
